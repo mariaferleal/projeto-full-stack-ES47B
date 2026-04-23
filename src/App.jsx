7edbabf785139f235dea2a39ddce15ce44e49bea
Maria Fernanda Leal Pinheiro <85962221+mariaferleal@users.noreply.github.com>
2026-04-22 22:59:20 -0300

    setup inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <main className="app-container">
+      <header className="app-header">
+        <h1>Buscador Rick and Morty</h1>
+        <p>
+          By Maria Fernanda Leal Pinheiro.
+        </p>
+      </header>
+    </main>
+  )
+}
+
+export default App
